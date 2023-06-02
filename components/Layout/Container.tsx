7173e8c665834c3f676299807465e20415b8fe4d
Rob Taylor <roboncode@gmail.com>
2023-06-02 13:31:09 -0600

    updated container component

--- components/Layout/Container.tsx
+++ components/Layout/Container.tsx
@@ -1,12 +1,12 @@
+import { splitProps } from "solid-js"
+
 export default function (props: any) {
-  let { class: className, contentClass, children, ...rest } = props
-  className = `flex flex-col overflow-hidden ${className}`
-  const contentStyles = `flex flex-col flex-grow h-0 overflow-auto  ${contentClass ?? ''}`
+  let [local, rest] = splitProps(props, ['class', 'classList', 'contentClass', 'children'])
+  const contentStyles = () => `flex flex-col flex-grow h-0 overflow-auto  ${local.contentClass ?? ''}`
 
   return (
-    <div class={className} {...rest}>
-      <div class={contentStyles}>{children}</div>
+    <div class={`flex flex-col overflow-hidden ${local.class}`} classList={local.classList} {...rest}>
+      <div class={contentStyles()}>{local.children}</div>
     </div>
   )
 }
-
