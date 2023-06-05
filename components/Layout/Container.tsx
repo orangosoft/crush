8bf61576c45ecfb0781fe37c1f146b43f0d37d06
Rob Taylor <roboncode@gmail.com>
2023-06-05 14:05:37 -0600

    added ability to disable a container

--- components/Layout/Container.tsx
+++ components/Layout/Container.tsx
@@ -1,8 +1,16 @@
-import { onCleanup, onMount, splitProps } from "solid-js"
+import { createEffect, onCleanup, onMount, splitProps } from "solid-js"
 
-export default function (props: { autoScroll?: Function, class?: string, classList?: any, contentClass?: string, children?: any }) {
+export default function (props: {
+  autoScroll?: Function,
+  class?: string,
+  classList?: any,
+  contentClass?: string,
+  children?: any,
+  disabled?: boolean,
+  [key: string]: any
+}) {
   let contentRef: any
-  let [local, rest] = splitProps(props, ['autoScroll', 'class', 'classList', 'contentClass', 'children'])
+  let [local, rest] = splitProps(props, ['autoScroll', 'class', 'classList', 'contentClass', 'children', 'disabled'])
   const contentStyles = () => `flex flex-col flex-grow h-0 overflow-auto  ${local.contentClass ?? ''}`
 
   if (local.autoScroll) {
@@ -24,6 +32,14 @@ export default function (props: { autoScroll?: Function, class?: string, classLi
       contentRef.scrollTo(0, 0)
     })
   }
+
+  createEffect(() => {
+    if (local.disabled) {
+      contentRef.setAttribute('inert', '')
+    } else {
+      contentRef.removeAttribute('inert')
+    }
+  })
 
   return (
     <div class={`flex flex-col overflow-hidden ${local.class}`} classList={local.classList} {...rest}>
