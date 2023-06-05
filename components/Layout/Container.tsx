cdcc1e6ea34d539abfac9acaa2551220cc3ea25c
Rob Taylor <roboncode@gmail.com>
2023-06-05 10:38:39 -0600

    added autoscroll to container

--- components/Layout/Container.tsx
+++ components/Layout/Container.tsx
@@ -1,12 +1,33 @@
-import { splitProps } from "solid-js"
+import { onCleanup, onMount, splitProps } from "solid-js"
 
-export default function (props: any) {
-  let [local, rest] = splitProps(props, ['class', 'classList', 'contentClass', 'children'])
+export default function (props: { autoScroll?: Function, class?: string, classList?: any, contentClass?: string, children?: any }) {
+  let contentRef: any
+  let [local, rest] = splitProps(props, ['autoScroll', 'class', 'classList', 'contentClass', 'children'])
   const contentStyles = () => `flex flex-col flex-grow h-0 overflow-auto  ${local.contentClass ?? ''}`
+
+  if (local.autoScroll) {
+    onMount(() => {
+      const handleUrlChange = () => {
+        contentRef.scrollTo(0, 0)
+      }
+
+      window.addEventListener('pushstate', handleUrlChange)
+      window.addEventListener('popstate', handleUrlChange)
+
+      onCleanup(() => {
+        window.removeEventListener('pushstate', handleUrlChange)
+        window.removeEventListener('popstate', handleUrlChange)
+      })
+    })
+
+    local.autoScroll(() => {
+      contentRef.scrollTo(0, 0)
+    })
+  }
 
   return (
     <div class={`flex flex-col overflow-hidden ${local.class}`} classList={local.classList} {...rest}>
-      <div class={contentStyles()}>{local.children}</div>
+      <div ref={contentRef} class={contentStyles()}>{local.children}</div>
     </div>
   )
 }
